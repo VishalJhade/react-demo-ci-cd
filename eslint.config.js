55806d528fcb0338f0400c8830175302f308de74
Vishal <vsjhade@gmail.com>
2025-06-12 05:44:18 +0530

    added tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -17,17 +18,25 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+   rules: {
+  ...js.configs.recommended.rules,
+  ...react.configs.recommended.rules,
+  ...reactHooks.configs.recommended.rules,
+  'react/react-in-jsx-scope': 'off', // 👈 Disable old rule
+  'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+  'react-refresh/only-export-components': [
+    'warn',
+    { allowConstantExport: true },
+  ],
+},
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-]
+];
